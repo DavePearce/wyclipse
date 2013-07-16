35425883c6efdf92e4ff6b3e739c784abc7b714c
DavePearce <david.pearce@ecs.vuw.ac.nz>
2013-07-17 08:15:27 +1200

    Working on the wyclipse plugin.

---
+++ plugins/wyclipse.core/src/wyclipse/ui/PreferencePage.java
@@ -0,0 +1,21 @@
+package wyclipse.ui;
+
+import org.eclipse.jface.preference.FieldEditorPreferencePage;
+import org.eclipse.ui.IWorkbench;
+import org.eclipse.ui.IWorkbenchPreferencePage;
+
+public class PreferencePage extends FieldEditorPreferencePage implements
+		IWorkbenchPreferencePage {
+
+	@Override
+	public void init(IWorkbench workbench) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	protected void createFieldEditors() {
+		// TODO Auto-generated method stub
+		
+	}
+}
