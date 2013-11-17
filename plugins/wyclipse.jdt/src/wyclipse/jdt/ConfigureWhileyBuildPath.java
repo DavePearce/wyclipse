19ae518e49aa1c657cf716b7e4ca9598054f5c70
DavePearce <david.pearce@ecs.vuw.ac.nz>
2013-11-18 10:57:21 +1300

    wyclipse.jdt: working on getting ConfigureWhileyBuildPath to recognise when there is a WhileyNature already.

--- plugins/wyclipse.jdt/src/wyclipse/jdt/ConfigureWhileyBuildPath.java
+++ plugins/wyclipse.jdt/src/wyclipse/jdt/ConfigureWhileyBuildPath.java
@@ -5,6 +5,8 @@ import org.eclipse.core.commands.ExecutionException;
 import org.eclipse.core.commands.IHandler;
 import org.eclipse.core.commands.IHandlerListener;
 import org.eclipse.core.resources.IProject;
+import org.eclipse.core.resources.IProjectDescription;
+import org.eclipse.core.runtime.CoreException;
 import org.eclipse.jface.viewers.ISelection;
 import org.eclipse.jface.viewers.IStructuredSelection;
 import org.eclipse.ui.handlers.HandlerUtil;
@@ -34,17 +36,28 @@ public class ConfigureWhileyBuildPath implements IHandler {
 		
 		// First, determine whether this command was executed on a selected
 		// project or not.
+		
+		try {
 		ISelection selection = HandlerUtil.getActiveWorkbenchWindow(event)
 				.getActivePage().getSelection();
 		if (selection != null & selection instanceof IStructuredSelection) {
 			IStructuredSelection iss = (IStructuredSelection) selection;
 			Object firstElement = iss.getFirstElement();
 			if (firstElement instanceof IProject) {
+
 				IProject project = (IProject) firstElement;
-				System.out.println("*** CALLED ON IPROJECT "
-						+ project.getName());
+				System.out.println("*** CALLED ON IPROJECT " + project.getName());
+				if(hasWhileyNature(project)) {
+					System.out.println("*** PROJECT HAS WHILEY NATURE");
+				} else {
+					System.out.println("*** PROJECT DOESN'T HAVE WHILEY NATURE");
+				}												
 			}
 		}
+		} catch(CoreException e) {
+			throw new ExecutionException("A problem occurred configuring the Whiley Build Path",e);
+		}
+		
 		return null;
 	}
 
@@ -62,4 +75,14 @@ public class ConfigureWhileyBuildPath implements IHandler {
 	@Override
 	public void removeHandlerListener(IHandlerListener handlerListener) { }
 
+	private boolean hasWhileyNature(IProject project) throws CoreException {
+		IProjectDescription description = project.getDescription();
+		for (String natureId : description.getNatureIds()) {
+			if (natureId.equals(wyclipse.core.Activator.WYCLIPSE_NATURE_ID)) {
+				return true;
+
+			}
+		}
+		return false;
+	}
 }
