fca2b563695c63066793cb09880010349c5c5379
DavePearce <david.pearce@ecs.vuw.ac.nz>
2013-11-19 10:52:12 +1300

    Very minor tweak

--- plugins/wyclipse.ui/src/wyclipse/ui/editor/ColorManager.java
+++ plugins/wyclipse.ui/src/wyclipse/ui/editor/ColorManager.java
@@ -60,7 +60,8 @@ public class ColorManager {
 //	
 	public static final Color KEYWORD_COLOR_C = new Color(null, 127, 0, 85);
 	//public static final Color SPEC_KEYWORD_COLOR_C = new Color(null, 135,95,1);
-	public static final Color SPEC_KEYWORD_COLOR_C = new Color(null, 100,75,0);
+	//public static final Color SPEC_KEYWORD_COLOR_C = new Color(null, 100,75,0);
+	public static final Color SPEC_KEYWORD_COLOR_C = new Color(null, 127, 0, 85);
 	public static final Color DEFAULT_COLOR_C = new Color(null, 0, 0, 0);
 	public static final Color STRING_COLOR_C = new Color(null, 42, 0, 255);
 	public static final Color COMMENT_COLOR_C = new Color(null, 63, 127, 95);
